3adc9f6a79bf9f468243e2cddd4e402a33743c8d
Mishaj__k <amstig100@gmail.com>
2024-07-15 10:58:09 +0530

    feat:signup back end

---
+++ server/logic.js
@@ -0,0 +1,40 @@
+let profile_imgs_name_list = [
+  "Garfield",
+  "Tinkerbell",
+  "Annie",
+  "Loki",
+  "Cleo",
+  "Angel",
+  "Bob",
+  "Mia",
+  "Coco",
+  "Gracie",
+  "Bear",
+  "Bella",
+  "Abby",
+  "Harley",
+  "Cali",
+  "Leo",
+  "Luna",
+  "Jack",
+  "Felix",
+  "Kiki",
+];
+
+let profile_imgs_collections_list = [
+  "notionists-neutral",
+  "adventurer-neutral",
+  "fun-emoji",
+];
+
+console.log(
+  `https://api.dicebear.com/6.x/${
+    profile_imgs_collections_list[
+      Math.floor(Math.random() * profile_imgs_collections_list.length)
+    ]
+  }/svg?seed=${
+    profile_imgs_name_list[
+      Math.floor(Math.random() * profile_imgs_name_list.length)
+    ]
+  }`
+);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config'
-import bcrypt from 'bcrypt';
+import "dotenv/config";
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+
+//schema
+import User from "./schema/user.js";
+
 
 const server = express();
 let PORT = 3001;
@@ -14,43 +19,76 @@ let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for pass
 server.use(express.json());
 
 mongoose.connect(process.env.DB_LOCATION, {
-    autoIndex:true
+  autoIndex: true,
 });
 
-server.post('/signup', (req, res) => {
-    let {fullname, email, password} = req.body;
+const formatDatatoSend = (user) => {
+  return {
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
 
-    //validating the data from frontend
-    if(fullname.length < 3){
-      return res.status(403).json({"error":"Full name must be atleast 3 letter long"})
-    }
-    if(!email.length){
-      return res.status(403).json({"error": "Enter Email"})
-    }
-    if(!emailRegex.test(email)){
-      return res.status(403).json({"error":"Email is invalid"})
-    }
-    if(!passwordRegex.test(password)){
-      return res.status(403).json({"error":"Password should be 6 to 20 charecter long with a numberic, 1 lowercase and 1 uppercase letter"})
-    }
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
 
-    bcrypt.hash(password, 10, (err, hashed_password) => {
-      
-      let username = email.split("@")[0]; ''
+  let isUsernameNotUnique = await User.exists({
+    "personal_info.username": username,
+  }).then((result) => result);
 
-      let user = new User
+  //console.log(isUsernameNotUnique)
 
+  isUsernameNotUnique ? (username += nanoid().substring(0, 5)) : "";
 
+  return username;
+};
 
-      console.log(hashed_password)
-    })
+server.post("/signup", (req, res) => {
+  let { fullname, email, password } = req.body;
 
+  //validating the data from frontend
+  if (fullname.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Full name must be atleast 3 letter long" });
+  }
+  if (!email.length) {
+    return res.status(403).json({ error: "Enter Email" });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "Email is invalid" });
+  }
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({
+      error:
+        "Password should be 6 to 20 charecter long with a numberic, 1 lowercase and 1 uppercase letter",
+    });
+  }
 
-    return res.status(200).json({"status" : "okay"})
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
+    let username = await generateUsername(email);
+    ("");
+
+    let user = new User({
+      personal_info: { fullname, email, password: hashed_password, username },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json(formatDatatoSend(u));
+      })
+      .catch((err) => {
+        if (err.code == 11000) {
+          return res.status(500).json({ error: "email aleady exist" });
+        }
+
+        return res.status(500).json({ error: err.message });
+      });
+  });
 });
 
 server.listen(PORT, () => {
   console.log("listening on port ->" + PORT);
 });
-
-
